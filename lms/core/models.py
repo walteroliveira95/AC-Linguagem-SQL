575934ec8ca8d340e635bd97918e263bf027d066
Vitor Pereira de Abreu <1801820@salas.aulas>
2018-10-05 20:50:07 -0300

    Add Aluno model

--- lms/core/models.py
+++ lms/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    id_usuario = models.ForeignKey('', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    celular = models.CharField(max_length=14)
+    ra = models.IntegerField()
+    foto = models.CharField(default=None, max_length=500)

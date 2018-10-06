bcd129b23fe471edbf4fde5e7e534a73e444a255
Vitor Pereira de Abreu <1801820@SALAS.AULAS>
2018-10-05 21:38:54 -0300

    add modules Usuario, Professor, Atividade

--- lms/core/models.py
+++ lms/core/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
+class Usuario(models.Model):
+    login = models.CharField(max_length=150)
+    senha = models.CharField(max_length=150)
+    dt_expiracao = models.DateTimeField()
+
 class Aluno(models.Model):
-    id_usuario = models.ForeignKey('', on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     celular = models.CharField(max_length=14)
     ra = models.IntegerField()
     foto = models.CharField(default=None, max_length=500)
+
+class Professor(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    email = models.CharField(max_length=200)
+    celular = models.CharField(max_length=14)
+    contato = models.CharField(max_length=150)
+
+class Atividade(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.CharField(max_length=250)
+    tipo = models.CharField(max_length=15)
+    id_professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    descricao = models.CharField(default=None, max_length=150)
+    extras = models.CharField(default=None, max_length=50)
